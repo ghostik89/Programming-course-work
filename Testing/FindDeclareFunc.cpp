1fe1085b181ddd6c8e21adf63c6082fb9a2da6a9
Lady_Proger <facktartar@mail.com>
2020-01-11 15:25:46 +0400

    edited tests in FindDeclareTest

--- Testing/FindDeclareFunc.cpp
+++ Testing/FindDeclareFunc.cpp
@@ -51,17 +51,62 @@ namespace FindDeclareTest
 		}
 
 		TEST_METHOD(DeclareTwoFuncs) {
-			const int Rows = 14;
+			const int Rows = 13;
 			const char SourceCode[Rows][MAX_LENGTH] = {
-				"void count();",
-				"int main()",
+				"void minus()",
 				"{",
-				"int a = 0;",
-				"return 0;"
-				"}"
+				"int b = 0;",
+				"}",
 				"void count()",
 				"{",
 				"int a = 0;",
+				"}",
+				"int main()",
+				"{",
+				"int i = 0;",
+				"return 0;",
+				"}"
+			};
+			const char NameOfFunc[MAX_LENGTH] = { "count" };
+			const int WaitingResult = 4;
+			int result = FindDeclareFunc(SourceCode, NameOfFunc, Rows);
+			Assert::AreEqual(result, WaitingResult);
+		}
+		
+		TEST_METHOD(DeclareTwoFuncsOneAfterMain) {
+			const int Rows = 14;
+			const char SourceCode[Rows][MAX_LENGTH] = {
+				"void minus()",
+				"{",
+				"int b = 0;",
+				"}",
+				"void count();",
+				"int main()",
+				"{",
+				"int i = 0;",
+				"return 0;",
+				"}",
+				"void count()",
+				"{",
+				"int a = 0;",
+				"}"
+			};
+			const char NameOfFunc[MAX_LENGTH] = { "count" };
+			const int WaitingResult = 4;
+			int result = FindDeclareFunc(SourceCode, NameOfFunc, Rows);
+			Assert::AreEqual(result, WaitingResult);
+		}
+		TEST_METHOD(VariableNameIsFuncName) {
+			const int Rows = 9;
+			const char SourceCode[Rows][MAX_LENGTH] = {
+				"void count()",
+				"{",
+				"int a = 0;",
+				"}",
+				"int main()",
+				"{",
+				"int count = 0;",
+				"return 0;"
 				"}"
 			};
 			const char NameOfFunc[MAX_LENGTH] = { "count" };
